7fe5e05cfa379427601e9e46f14e872f4de770a0
phpc0de <46225600+phpc0de@users.noreply.github.com>
2020-12-24 01:48:06 +0800

    Update demo.sh

--- mini/include/demo.sh
+++ mini/include/demo.sh
@@ -5,6 +5,7 @@ DEMO() {
   pushd ${oneinstack_dir}/src > /dev/null
   if [ ! -e ${wwwroot_dir}/default/index.html ]; then 
     /bin/cp ${oneinstack_dir}/config/index.html ${wwwroot_dir}/default
+    /bin/cp ${oneinstack_dir}/config/ocp.php  ${wwwroot_dir}/default
   fi
 
   chown -R ${run_user}.${run_group} ${wwwroot_dir}/default
