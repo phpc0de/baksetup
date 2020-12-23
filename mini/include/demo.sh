dfa3ec3d628f2719135eb7435b46029f4d5bb6fb
phpc0de <46225600+phpc0de@users.noreply.github.com>
2020-12-24 01:11:48 +0800

    Create demo.sh

---
+++ mini/include/demo.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+
+DEMO() {
+  pushd ${oneinstack_dir}/src > /dev/null
+  if [ ! -e ${wwwroot_dir}/default/index.html ]; then 
+    /bin/cp ${oneinstack_dir}/config/index.html ${wwwroot_dir}/default
+  fi
+
+  chown -R ${run_user}.${run_group} ${wwwroot_dir}/default
+  [ -e /bin/systemctl ] && systemctl daemon-reload
+  popd > /dev/null
+}
