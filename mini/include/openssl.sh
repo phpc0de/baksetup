9c817e79cf33cdd0a777a9cd9646e226be2ec4c4
phpc0de <46225600+phpc0de@users.noreply.github.com>
2020-12-23 15:47:34 +0800

    Create openssl.sh

---
+++ mini/include/openssl.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+# Author:  yeho <lj2007331 AT gmail.com>
+
+
+Install_openSSL() {
+  if [ -e "${openssl_install_dir}/lib/libssl.a" ]; then
+    echo "${CWARNING}openSSL already installed! ${CEND}"
+  else
+    pushd ${oneinstack_dir}/src > /dev/null
+    tar xzf openssl-${openssl_ver}.tar.gz
+    pushd openssl-${openssl_ver} > /dev/null
+    make clean
+    ./config -Wl,-rpath=${openssl_install_dir}/lib -fPIC --prefix=${openssl_install_dir} --openssldir=${openssl_install_dir}
+    make depend
+    make -j ${THREAD} && make install
+    popd > /dev/null
+    if [ -f "${openssl_install_dir}/lib/libcrypto.a" ]; then
+      echo "${CSUCCESS}openssl installed successfully! ${CEND}"
+      /bin/cp cacert.pem ${openssl_install_dir}/cert.pem
+      rm -rf openssl-${openssl_ver}
+    else
+      echo "${CFAILURE}openSSL install failed, Please contact the author! ${CEND}" && lsb_release -a
+      kill -9 $$
+    fi
+    popd > /dev/null
+  fi
+}
