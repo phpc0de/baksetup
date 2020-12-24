df1c0781c8c710c485ea23a8d81b4899319c1405
phpc0de <46225600+phpc0de@users.noreply.github.com>
2020-12-24 19:15:02 +0800

    Update openssl.sh

--- mini/include/openssl.sh
+++ mini/include/openssl.sh
@@ -7,8 +7,8 @@ Install_openSSL() {
     echo "${CWARNING}openSSL already installed! ${CEND}"
   else
     pushd ${oneinstack_dir}/src > /dev/null
-    tar xzf openssl-${openssl_ver}.tar.gz
-    pushd openssl-${openssl_ver} > /dev/null
+    tar xzf OpenSSL_${openssl11_ver}.tar.gz
+    pushd OpenSSL_${openssl_ver} > /dev/null
     make clean
     ./config -Wl,-rpath=${openssl_install_dir}/lib -fPIC --prefix=${openssl_install_dir} --openssldir=${openssl_install_dir}
     make depend
@@ -17,7 +17,7 @@ Install_openSSL() {
     if [ -f "${openssl_install_dir}/lib/libcrypto.a" ]; then
       echo "${CSUCCESS}openssl installed successfully! ${CEND}"
       /bin/cp cacert.pem ${openssl_install_dir}/cert.pem
-      rm -rf openssl-${openssl_ver}
+      rm -rf OpenSSL_${openssl_ver}
     else
       echo "${CFAILURE}openSSL install failed, Please contact the author! ${CEND}" && lsb_release -a
       kill -9 $$
